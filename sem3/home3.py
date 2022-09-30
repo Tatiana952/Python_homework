fb0dd5d62eb1c64a7df5783fd3a01aca180b74bd
Tatiana <lulu.kep@yandex.ru>
2022-10-01 00:29:23 +0400

    home3

---
+++ sem3/home3.py
@@ -0,0 +1,23 @@
+s = int(input('Size of list = '))
+
+lst = []
+
+for i in range(s):
+    lst.append(float(input(f'lst[{i}] = ')))
+
+
+max_ = round(lst[0] % 1, 7)
+min_ = round(lst[0] % 1, 7)
+
+for i in range(s):
+    k = round(lst[i] % 1, 7)
+    if k != 0:
+        if max_ < k:
+            max_ = k
+
+        elif min_ > k:
+            min_ = k
+
+print(min_)
+print(max_)
+print(round((max_ - min_), 7))
