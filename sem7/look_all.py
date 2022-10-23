0e6ec846d68a9b9ecdc1ff1c7489517d82488f9e
Tatiana <lulu.kep@yandex.ru>
2022-10-23 23:26:47 +0400

    base

---
+++ sem7/look_all.py
@@ -0,0 +1,12 @@
+
+
+def show():
+    lst = []
+    with open('Base.txt', 'r', encoding='utf-8') as b:
+        k = b.readlines()
+    k = list(map(lambda x: x.replace('\n', ''), k))
+    print()
+    for i in range(len(k)): 
+        print(k[i])
+
+
