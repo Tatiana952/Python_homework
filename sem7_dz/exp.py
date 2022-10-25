5f475408d12fc464e01f3353d8f8c056ec3f43d1
Tatiana952 <107501679+Tatiana952@users.noreply.github.com>
2022-10-25 17:23:51 +0400

    1

--- sem7_dz/exp.py
+++ sem7_dz/exp.py
@@ -1,7 +1,5 @@
-import csv
 
 def export():
-    lst = []
     with open('Base.txt', 'r', encoding='utf-8') as b:
         k = b.readlines()
     k = list(map(lambda x: x.replace('\n', ''), k))
@@ -17,21 +15,15 @@ def export():
                 else:
                     file.write(k[i] + ', ')
     elif f == 2:
-        d = {0: 'Surname: ',
-        1: 'Name: ',
-        2: 'Telephone num.: ',
-        3: 'Description: '}
         style = 'style="font-size:15px;"'
         html = '<html>\n    <head></head>\n       <body>\n'
         for i in range(len(k)):
-            html += '           <p {}> {} </p>\n'\
-                .format(style,k[i])
-            # html += '           <p {}>Wind speed: {} m/s</p>\n'\
-            #     .format(style,wind_speed_view(device))
-            # html += '           <p {}>Pressure: {} mmHg</p>\n'\
-            #     .format(style,pressure_view(device))
+            if k[i] == '':
+                html += '           <p {}>~~~~~~~~~{} </p>\n'\
+                    .format(style,k[i])
+            else:
+                html += '           <p {}> {} </p>\n'\
+                    .format(style,k[i])
         html += '      </body>\n</html>'
-        with open('Export.html', 'w') as file:
+        with open('Export.html', 'w', encoding = 'utf-8') as file:
             file.write(html)
-            
-
