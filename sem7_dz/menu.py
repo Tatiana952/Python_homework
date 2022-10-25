95dc23f6db21c707f52b422ab6b2065651070eec
Tatiana952 <107501679+Tatiana952@users.noreply.github.com>
2022-10-25 17:24:08 +0400

    5

--- sem7_dz/menu.py
+++ sem7_dz/menu.py
@@ -1,37 +1,23 @@
-from get_num import get_numbers
-from operations import mult
-from operations import sum
-from operations import diff
-from operations import div
+import imp
+from  exp import export
+from im import import_
+from look_all import show
+from add_con import add
 
-print('This is a calculator. It supports operations like: *, /, +, -')
+b = False
 
-end = False
+while b == False: 
+    print("\nWhat do you want to do?")
+    print("1. Show all contacts\n2. Add contact\n3. Import contacts\n4. Export contacts\n5. End programm")
+    p = int(input('Answer = '))
 
-while end == False:
-    r = get_numbers()
-    #print(r)
-
-    if r[0] == '1':
-        print('\nWhat operation do you choose?\n\
-        1. a + b   2. a - b   3. a * b   4. a / b')
-        op = int(input('Operation = '))
-    else:
-        print('\nWhat operation do you choose?\n\
-        1. a + b   2. a - b   3. a * b')
-        op = int(input('Operation = '))
-    
-    if op == 3:
-        mult(r)
-    elif op == 1:
-        sum(r)
-    elif op == 2:
-        diff(r)
-    else:
-        div(r)
-
-    wish = input('\nDo you want to continue?\n1.Yes    2.No\nAnswer = ')
-    if int(wish) == 1:
-        end = False
-    else:
-        end = True
+    if p == 5:
+        b = True
+    elif p == 1:
+        show()
+    elif p == 2:
+        add()
+    elif p == 3:
+        import_()
+    elif p == 4:
+        export()
