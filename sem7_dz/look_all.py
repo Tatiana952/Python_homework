6b24dc541f9cdd68c2cd00372e8a585e4b4bbdde
Tatiana952 <107501679+Tatiana952@users.noreply.github.com>
2022-10-25 17:28:13 +0400

    5

--- sem7_dz/look_all.py
+++ sem7_dz/look_all.py
@@ -1,12 +1,9 @@
 
 
 def show():
-    lst = []
     with open('Base.txt', 'r', encoding='utf-8') as b:
         k = b.readlines()
     k = list(map(lambda x: x.replace('\n', ''), k))
     print()
     for i in range(len(k)): 
         print(k[i])
-
-
